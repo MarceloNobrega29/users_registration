7d93d207675e41d2e5f51c981752be0ea79b08bb
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-09-30 11:05:53 -0300

    Initial commit

---
+++ src/pages/Home/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './style.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <form action="">
+        <h1>Users Registration</h1>
+        <input name='name' type="text" />
+        <input age='age' type="number" />
+        <input email='name' type="email" />
+        <button type='button'>Registration</button>
+      </form>
+
+      <div>
+          
+      </div>      
+    </div>
+  )
+}
+
+export default App
