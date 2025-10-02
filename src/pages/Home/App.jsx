c8a7ee87782d1a78cb6fec5490bd3c07da265d45
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-10-02 20:14:53 -0300

    feat: alguns testes e adicionando o css inicial

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 const App = () => {
+
+  const users = [
+    {
+      id: '2323aa',
+      name: 'marcelo',
+      age: '19',
+      email: 'mf@gmail.com'
+    },
+    { 
+      id: '2323a321a',
+      name: 'teste',
+      age: '42',
+      email: 'ttf@gmail.com'
+    }
+  ]
+
+
+
   return (
     <div className='container'>
       <form action="">
@@ -12,9 +31,23 @@ const App = () => {
         <button type='button'>Registration</button>
       </form>
 
-      <div>
-          
-      </div>      
+    { users.map( (user) => (
+
+      <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>          
+          <button>
+            <img src={Trash}/>
+          </button>
+      </div>     
+
+    ) )}
+
+       
+
     </div>
   )
 }
